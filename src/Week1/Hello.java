6567abc205aa7010f6b09fc38ee7762b404a1836
0woy <qkrdbsdk1030@naver.com>
2023-03-23 15:18:48 +0900

    출력 예제

---
+++ src/Week1/Hello.java
@@ -0,0 +1,22 @@
+package Week1;
+
+public class Hello {
+		
+	public static int sum(int n, int m) {
+		return n+m;
+	}
+
+	public static void main(String[] args) {
+		int i=20;
+		int s;
+		char a;
+		
+		s = sum(i,20);
+		a='?';
+
+		System.out.print(a);
+		System.out.println("Hello");
+		System.out.print(s);
+
+	}
+}
